1ff136c7da196f32e533cc9c37c1e15f4172787b
MasonManca <71055156+MasonManca@users.noreply.github.com>
2021-10-12 14:34:29 -0700

    DA4 notes

---
+++ Cleaning.py
@@ -0,0 +1,15 @@
+"""
+
+ways to handle missing values
+    Remove them
+        Used when having thousands of instances and few NAs
+    Fill or replace the missing values with valid values
+        by hand
+        use a central tendency measure, mean median or mode (interpulate() from pandas)
+    Machine learning to make a prediction
+    ignore them
+    fill na 
+    lfill bfill
+    .isnull.sum() determines how many NAs
+    
+"""
