e90a1c402e87978350edac6601b14462125f6126
MasonManca <71055156+MasonManca@users.noreply.github.com>
2021-10-14 13:27:42 -0700

    notes

--- U4Notes.py
+++ U4Notes.py
@@ -3,5 +3,6 @@ JSON- Java script object notation
 API- application programming interface
     A set of functions that a programmer exposes for other programmers to use
     w/o needing to know how the functions are implemented
+    use cases: get and send data
     
 """
