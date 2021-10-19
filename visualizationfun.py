259a5b78e64b664f0444e5b29208a2754dcc5765
Manca <mmanca2@hk223-27704.gonzaga.edu>
2021-10-19 14:57:27 -0700

    notes

--- visualizationfun.py
+++ visualizationfun.py
@@ -1,9 +1,43 @@
-
-
-
-
-'''
+import matplotlib.pyplot as plt
 
 
 
 '''
+EDA- explatory data analysis
+getting familiar with your data
+    exploring it
+
+goals:
+    1) clearly and accurately represent data
+    2) be creative, with the goal of increasing readability and understanding
+    3) Label the units and points of interest
+
+some jargon
+chart: a 2d visualization
+plot: a chart with data points (e.g. scatter plot)
+graph: a chart for a mnathfunction (e.g. sine)
+
+we will use the matplotlib library for charting w/python
+
+3 ways to use matplotlib
+1) using the pyplot module (what data scientists typically use)
+*there is always a "current" figure*
+2) using the oop interface
+3) a mix of the first two
+
+'''
+
+def line_chart_example(x,y):
+    
+
+def main():
+    x = list(range(6))
+    y = []
+
+    for value in x:
+        y.append(val**2)
+    print(x)
+    print(y)
+    line_chart_example(x, y)
+
+main()
