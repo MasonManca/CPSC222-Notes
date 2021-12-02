0a892ee76d1574fbe95b7255a2aa9fea7116ffaf
MasonManca <71055156+MasonManca@users.noreply.github.com>
2021-12-02 14:00:06 -0800

    notes

---
+++ 4-11-21Notes.py
@@ -0,0 +1,22 @@
+
+
+'''
+Normal Distributions:
+    * mean = median = mode
+    * Symmetric about the center
+        Can apply conclusions made on one side to the other side
+    * 50% less than mean and more than mean
+    * Area under the curve is 1
+    Given a sample of observations, we can compute many standard deviations a single observation x, is form the mean
+    Z score - x- mean/ stdv
+T-Distribution:
+    *Like a Z distribution based on a certain number called the degrees of freedom.
+        * n is the sample size
+            *as n increases, so does your degrees of freedom, making a t distribution looking more and more like a z distribution
+        
+    -When to use a t-dist instead of a z-dist
+        *Sample size is small < 30
+        *When you don't know your population standard deviation (sigma)
+        *When a normal distribution it is safe to use a t-dist
+        
+'''
